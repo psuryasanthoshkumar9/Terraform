6d3a869bf68efe77d83efec1ae2a248480c33a26
P Surya Santhosh Kumar <123743514+psuryasanthoshkumar9@users.noreply.github.com>
2025-11-23 15:36:45 +0530

    Update main.tf

--- Day-1-basic-code/main.tf
+++ Day-1-basic-code/main.tf
@@ -1,15 +1,17 @@
 resource "aws_vpc" "dev" {
-    cidr_block = "10.0.0.0/16"
-    tags = {
-Name = "dev_VPC"
-    }
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "dev_VPC"
   }
-  resource "aws_vpc" "test" {
-    cidr_block = "10.0.0.0/16"
-    tags = {
-Name = "dev_VPC"
-    }
+}
+
+resource "aws_subnet" "dev_subnet" {
+  vpc_id     = aws_vpc.dev.id
+  cidr_block = "10.0.1.0/24"   # <<< THIS IS REQUIRED
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "dev_subnet"
   }
-  resource "aws_subnet" "dev_subnet" {
-    vpc_id = aws_vpc.dev.id
-  }
+}
